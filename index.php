459ac0edb0d182ad1f81c321e571f5b43a5662e5
yotsawatt <c.yotsawat.ed@gmail.com>
2019-12-20 15:28:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HC Group Thailands</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include('layout/navbar.php') ?>
+
+    <!-- Section 1 : Present company and slide show -->
+    <div class="section-1">
+        <!-- left side -->
+        <div class="present-leftside">
+            <div class="box-content">
+                <p class="first-content-text">Ginger Product</p>
+                <h1 class="second-content-text">Hcgroup <span class="orange">Thailand</span></h1>
+                <p class="third-content-text">We started our business to offering high quality ingredients to food and beverage </p>
+            </div>
+            <div class="box-button">
+                <button class="btn">Read More</button>
+                <button class="btn btn-reverse">Order Now</button>
+            </div>
+        </div>
+        <!-- right side -->
+        <div class="present-rightside">
+            <img src="https://images.unsplash.com/photo-1535850579364-952ef600d22e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1292&q=80" alt="">
+        </div>
+    </div>
+
+    <!-- Section 2 : About us section -->
+    <div class="section-2">
+        <!-- left side -->
+        <div class="aboutus-leftside">
+            <img src="https://images.unsplash.com/photo-1518205546272-7b708b0a0020?ixlib=rb-1.2.1&auto=format&fit=crop&w=981&q=80" alt="">
+        </div>
+        <!-- right side -->
+        <div class="aboutus-rightside">
+            <div class="box-content">
+                <p class="first-content-text">About Us</p>
+                <h1 class="second-content-text">Welcome to <span class="orange">Ginger land</span></h1>
+                <p class="third-content-text">We started our business to offering high quality ingredients to food and beverage </p>
+            </div>
+            <div class="box-button">
+                <button class="btn btn-reverse">Read More</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Section 3 : Product Recommend -->
+    <div class="container">
+        <div class="section-3-header">
+            <p>Tasty</p>
+            <h1><span class="orange">Special</span> Recommend Product</h1>
+        </div>
+        <div class="section-3">
+            <div class="card-container">
+                <div class="card-img">
+                    <img src="https://images.unsplash.com/photo-1527903825799-197bab1e7550?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+                </div>
+                <div class="card-box-content">
+                    <h1>Product Name</h1>
+                    <p>More product detail</p>
+                    <button class="btn btn-small">View More</button>
+                </div>
+            </div>
+            <div class="card-container">
+                <div class="card-img">
+                    <img src="https://images.unsplash.com/photo-1527904288719-6be80b9c2415?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+                </div>
+                <div class="card-box-content">
+                    <h1>Product Name</h1>
+                    <p>More product detail</p>
+                    <button class="btn btn-small">View More</button>
+                </div>
+            </div>
+            <div class="card-container">
+                <div class="card-img">
+                    <img src="https://images.unsplash.com/photo-1527904248767-cfe951e81ab0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="">
+                </div>
+                <div class="card-box-content">
+                    <h1>Product Name</h1>
+                    <p>More product detail</p>
+                    <button class="btn btn-small">View More</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Section 4 : Product grid section -->
+    <div class="section-4">
+        <div class="container">
+            <div class="product-header">
+                <p>Product</p>
+                <h1>More <span class="orange">Powerful</span> Product</h1>
+            </div>
+            <div class="product-container">
+                <div class="product-item">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1528488973032-1d4818f491a8?ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" alt=""></a>
+                </div>
+                <div class="product-item">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1534531173927-aeb928d54385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""></a>
+                </div>
+                <div class="product-item">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1487376480913-24046456a727?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""></a>
+                </div>
+                <div class="product-item">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1468777675496-5782faaea55b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80" alt=""></a>
+                </div>
+                <div class="product-item">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1439127989242-c3749a012eac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""></a>
+                </div>
+                <div class="product-item">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1487376480913-24046456a727?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""></a>
+                </div>
+                <div class="product-item">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1468777675496-5782faaea55b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80" alt=""></a>
+                </div>
+                <div class="product-item">
+                    <a href="#"><img src="https://images.unsplash.com/photo-1439127989242-c3749a012eac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Section 6 : Other product -->
+    <div class="section-6">
+        <div class="other-leftside">
+            <div class="leftside-text">
+                <h1>Fureiko</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iure cum culpa dolores illo facere id nobis quos expedita hic voluptatum quis.</p>
+                <button class="btn btn-small">Go to Fureiko</button>
+            </div>
+        </div>
+        <div class="other-rightside">
+            <div class="rightside-text">
+                <h1>Nanoxan</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iure cum culpa dolores illo facere id nobis quos expedita hic voluptatum quis.</p>
+                <button class="btn btn-small">Go to Nanoxan</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- section 5: certificate section -->
+    <div class="section-5">
+        <div class="container">
+            <div class="cer-header">
+                <p>Certificate</p>
+                <h1>The best <span class="black">food and factory standatd</span></h1>
+            </div>
+           <div class="cer-container">
+                <div class="cer-item">
+                    <img src="https://images.unsplash.com/photo-1575897368738-aa5401c4af7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=641&q=80" alt="">
+                    <h1>ISO 9001</h1>
+                    <p>It a sertificate for factory</p>
+                    <button class="btn btn-small">Read More</button>
+                </div>
+                <div class="cer-item">
+                    <img src="https://images.unsplash.com/photo-1554350550-0453441c9e64?ixlib=rb-1.2.1&auto=format&fit=crop&w=647&q=80" alt="">
+                    <h1>BRC</h1>
+                    <p>It a sertificate for factory</p>
+                    <button class="btn btn-small">Read More</button>
+                </div>
+                <div class="cer-item">
+                    <img src="https://images.unsplash.com/photo-1554631492-a054feeae929?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="">
+                    <h1>HACCP</h1>
+                    <p>It a sertificate for factory</p>
+                    <button class="btn btn-small">Read More</button>
+                </div>
+                <div class="cer-item">
+                    <img src="https://images.unsplash.com/photo-1545816721-c2869d52442b?ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80" alt="">
+                    <h1>HALAL</h1>
+                    <p>It a sertificate for factory</p>
+                    <button class="btn btn-small">Read More</button>
+                </div>
+           </div>
+        </div>
+    </div>
+    
+    <!-- Footer -->
+    <footer>
+
+    </footer>
+</body>
+</html>
---
+++ layout/navbar.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="container-head">
+        <div class="logo">
+            <h1 class="header-logo">Hc Group</h1>
+        </div>
+        <ul class="navbar">
+            <li><a href="">homepage</a></li>
+            <li><a href="">product</a></li>
+            <li><a href="">about us</a></li>
+            <li><a href="">Find a store</a></li>
+            <li><a href="">shop online</a></li>
+            <li><a href="">contact us</a></li>
+        </ul>  
+    </div>
+</header>
