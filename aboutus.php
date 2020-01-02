95e3a192e1e7e60254b2f898e276f0388cb8cb19
yotsawatt <c.yotsawat.ed@gmail.com>
2020-01-02 16:54:56 +0700

    02-01-2020

--- aboutus.php
+++ aboutus.php
@@ -2,11 +2,70 @@
 <html lang="en">
 <head>
     <?php include('layout/include.php') ?>
+    <link rel="stylesheet" href="css/aboutus.css">
 </head>
 <body>
     <?php include('layout/navbar.php') ?>
     <div class="container">
-        <h1>About us</h1>
+        <div class="header-text">
+            <p>About us</p>
+            <h1>Get to know us</h1>
+        </div>
+        <!-- about us paragraph -->
+        <div class="section-1">
+            <p>HSU CHUAN FOODS Co.,Ltd. established in 1993 with main objective to export salted ginger to japan. In 2008 we expanded the factory land from 17,600 square meters to 156,800 square meters for support the growing productions with a capacity of product 10,000 metric tons annually.</p>
+            <p>Establishment of CHIANGRAI AGRO-INDUSRTY Co.,Ltd. in 2009 to produce and sell ready-to-eat products. The project was supported by Thailand Board of Investment (BOI).</p>
+            <p>Nowadays, our products are available to worldwide market with the aim to meet our consumer's satisfaction and safety. All this done under our policy : Impression, Safety and Quality</p>
+        </div>
+
+        <!-- key for success -->
+        <div class="header-text">
+            <p>About us</p>
+            <h1>Key for success</h1>
+        </div>
+        <div class="section-2">
+            <div class="key-for-container">
+                <div class="left-key">
+                    <img src="https://images.unsplash.com/photo-1571942794234-fecca8604664?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80" alt="">
+                </div>
+                <div class="right-key">
+                    <h1>Quality</h1>
+                    <p>All raw material are grown in Northern Thailand and controlled under GAP ( Good Agricultural practice ) with our closely suppliers.</p>
+                </div>
+            </div>
+            <div class="key-for-container">
+                <div class="left-key">
+                    <img src="https://images.unsplash.com/photo-1571942794234-fecca8604664?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80" alt="">
+                </div>
+                <div class="right-key">
+                    <h1>Quality</h1>
+                    <p>All raw material are grown in Northern Thailand and controlled under GAP ( Good Agricultural practice ) with our closely suppliers.</p>
+                </div>
+            </div>
+            <div class="key-for-container">
+                <div class="left-key">
+                    <img src="https://images.unsplash.com/photo-1571942794234-fecca8604664?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80" alt="">
+                </div>
+                <div class="right-key">
+                    <h1>Quality</h1>
+                    <p>All raw material are grown in Northern Thailand and controlled under GAP ( Good Agricultural practice ) with our closely suppliers.</p>
+                </div>
+            </div>
+        </div>
+        <!-- video section -->
+        <div class="section-3">
+            <div class="video-container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/kxAxJuJnMLQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+        <!-- certificate section -->
+        <div class="header-text">
+            <p>About us</p>
+            <h1>Certificate</h1>
+        </div>
+        <div class="section-4">
+
+        </div>
     </div>
 </body>
 </html>
