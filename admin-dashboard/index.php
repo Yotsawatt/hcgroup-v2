2bafe4a4e810627304535082dd4a54624e901f05
yotsawatt <c.yotsawat.ed@gmail.com>
2020-01-07 17:05:24 +0700

    07-01-2020

---
+++ admin-dashboard/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('layout/include.php') ?>
+</head>
+<body>
+    <div class="login-container">
+        <form action="" method="post">
+            <h1>Login</h1>
+            <input type="text" name="username" placeholder="username">
+            <input type="password" name="password" placeholder="password">
+            <input type="submit" value="login">
+        </form>
+    </div>
+</body>
+</html>
