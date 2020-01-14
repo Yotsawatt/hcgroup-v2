4bf0cfbe6260669ca5508f24e9676e05d31097d2
Yotsawatt <c.yotsawat.ed@gmail.com>
2020-01-14 20:07:53 +0700

    14-01-2020

--- admin-dashboard/index.php
+++ admin-dashboard/index.php
@@ -5,11 +5,11 @@
 </head>
 <body>
     <div class="login-container">
-        <form action="" method="post">
+        <form action="" method="post" class="login-form">
             <h1>Login</h1>
-            <input type="text" name="username" placeholder="username">
-            <input type="password" name="password" placeholder="password">
-            <input type="submit" value="login">
+            <input type="text" name="username" placeholder="username" class="form-input">
+            <input type="password" name="password" placeholder="password" class="form-input">
+            <input type="submit" value="login" class="btn-submit">
         </form>
     </div>
 </body>
--- admin-dashboard/mainpage.php
+++ admin-dashboard/mainpage.php
@@ -1,7 +1,4 @@
-<?php 
-    session_start();
-    require_once("controller/mainpage_con.php");
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
